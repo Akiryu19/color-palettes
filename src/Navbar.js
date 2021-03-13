8e4b572db89f019b2e175dbfd341f5d8ea532df5
AkiRyu19 <aki.riyuu@gmail.com>
2021-03-13 17:04:14 -0500

    add select and material UI

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import './Navbar.css';
+import Select from '@material-ui/core/Select';
+import { MenuItem } from '@material-ui/core';
 
 const Navbar = (props) => {
+  //   const [format, setFormat] = useState('hex');
+
+  //   const handleChange = (e) => {
+  //     setFormat(e.target.value);
+  //     props.changeFormat(e.target.value);
+  //   };
   return (
     <header className="Navbar">
       <div className="logo">
@@ -20,6 +28,13 @@ const Navbar = (props) => {
             onAfterChange={props.changeLevel}
           />
         </div>
+      </div>
+      <div className="select-container">
+        <Select value={props.format} onChange={props.changeFormat}>
+          <MenuItem value="hex">HEX - #ffffff</MenuItem>
+          <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+          <MenuItem value="rgba">RGBA - rgba(255,255,255,1)</MenuItem>
+        </Select>
       </div>
     </header>
   );
--- src/Palette.js
+++ src/Palette.js
@@ -1,21 +1,29 @@
 import React, { useState } from 'react';
 import ColorBox from './ColorBox';
 import Navbar from './Navbar';
-import Slider, { Range } from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import './Palette.css';
 
 const Palette = (props) => {
   const [level, setLevel] = useState(400);
+  const [format, setFormat] = useState('hex');
   const colorBoxes = props.Palette.colors[level].map((color) => (
-    <ColorBox background={color.hex} name={color.name} />
+    <ColorBox background={color[format]} name={color.name} />
   ));
   const changeLevel = (level) => {
     setLevel(level);
   };
+  const changeFormat = (e) => {
+    setFormat(e.target.value);
+  };
   return (
     <div className="Palette">
-      <Navbar level={level} changeLevel={changeLevel} />
+      <Navbar
+        level={level}
+        format={format}
+        changeLevel={changeLevel}
+        changeFormat={changeFormat}
+      />
 
       <div className="Palette-colors">{colorBoxes}</div>
     </div>
